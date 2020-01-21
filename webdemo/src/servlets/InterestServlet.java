8d06918585e8358fb17709257daddb2889451e3a
srikanthpragada <srikanthpragada@gmail.com>
2020-01-21 07:58:01 +0530

    21-JAN

--- webdemo/src/servlets/InterestServlet.java
+++ webdemo/src/servlets/InterestServlet.java
@@ -9,12 +9,27 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/interest")
 public class InterestServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      		double amount = Double.parseDouble(request.getParameter("amount"));
-      		double rate = Double.parseDouble(request.getParameter("rate"));
-      		
-      		double interest = amount * rate / 100;
-      		response.setContentType("text/html");
-      		response.getWriter().println("<h2>Interest = " + interest + "</h2>");
+
+	public void init() {
+		System.out.println("Initializing InterestServlet");
+	}
+
+	public void destory() {
+		System.out.println("Destroying InterestServlet");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		double amount = Double.parseDouble(request.getParameter("amount"));
+		double rate = Double.parseDouble(request.getParameter("rate"));
+
+		double interest = amount * rate / 100;
+		response.setContentType("text/html");
+		response.getWriter().println("<h2>Interest = " + interest + "</h2>");
 	}
 }
